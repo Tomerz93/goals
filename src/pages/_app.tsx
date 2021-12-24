65cf9b6a424279cf32bcc50078015f5879119a79
Tomer Zuker <tomerzuker@Tomers-MacBook-Pro.local>
2021-12-24 22:11:18 +0900

    added initial firebase setup and some base css classes as well as base authentication using google as provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import '../styles/globals.css'
+import '../styles/index.scss'
 import type { AppProps } from 'next/app'
+import { AuthProvider } from '@lib/context'
 import { Layout, Header } from "@components/UI"
-const MyApp = ({ Component, pageProps }: AppProps) =>
-  <>
-    <Layout>
-      <Header />
-      <Component {...pageProps} />
-    </Layout>
-  </>
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <AuthProvider>
+      <Layout>
+        <Header />
+        <Component {...pageProps} />
+      </Layout>
+    </AuthProvider>
+
+  )
+}
 
 
 export default MyApp
